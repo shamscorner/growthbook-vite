ae0eb877b3a4bb2c98b5a436736f7fc598df63c5
Shamim <shossain@secureship.ca>
2024-06-14 21:39:46 +0600

    chore: update homepage

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,6 +11,23 @@
 
 <h1>Growthbook Vite Edition</h1>
 
+<p>
+	This is an unofficial Growthbook library for Sveltekit projects until they release their official
+	one. This is based on their JS-sdk.
+</p>
+
+<p>
+	Check out the official website <a href="https://www.growthbook.io/" target="_blank">Growthbook</a
+	>.
+</p>
+
+<p>
+	Read more about this in their official <a
+		href="https://github.com/growthbook/growthbook"
+		target="_blank">Github repository</a
+	>.
+</p>
+
 <!-- {#if growthbook.isOn('my-feature')}
 	<p>My Feature is on!</p>
 {:else}
