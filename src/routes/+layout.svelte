b0e74cd7f4f7afad279fc82f5e7e1e55a6166922
Shamim <shossain@secureship.ca>
2024-06-25 18:11:36 +0600

    chore: update dependency setup and usage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,1 @@
-<script lang="ts">
-	import { onMount } from 'svelte';
-	import type { LayoutData } from './$types.js';
-
-	export let data: LayoutData;
-
-	onMount(() => {
-		console.log('Layout data:', data);
-	});
-</script>
-
 <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
-	import type { PageData } from './$types.js';
-
-	export let data: PageData;
-
-	onMount(async () => {
-		console.log('Page data:', data);
-	});
+	import { growthbook } from '../growthbook.js';
 </script>
 
 <h1>Growthbook Vite Edition</h1>
@@ -28,7 +22,7 @@
 	>.
 </p>
 
-<!-- {#if growthbook.isOn('my-feature')}
+{#if growthbook.isOn('my-feature')}
 	<p>My Feature is on!</p>
 {:else}
 	<p>My Feature is off!</p>
@@ -38,4 +32,4 @@
 	<p>My On Feature is on!</p>
 {:else}
 	<p>My On Feature is off!</p>
-{/if} -->
+{/if}
